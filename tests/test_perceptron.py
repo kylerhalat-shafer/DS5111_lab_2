44348c28bf9ef399c10bb6bfba446511abace8c0
Ubuntu <ubuntu@ip-172-31-4-96.us-east-2.compute.internal>
2023-10-09 16:07:21 +0000

    test perceptron

---
+++ tests/test_perceptron.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append(".")
+
+def test_perceptron():
+    the_perceptron = Perceptron()
+    the_perceptron.train([
+        [1,1],
+        [1,0],
+        [0,1],
+        [0,0],
+    ], [1,1,1,0])
+
+    assert the_perceptron.predict([1,1]) ==  1, "assert comment"
+    assert the_perceptron.predict([1,0]) ==  1, "assert comment"
+    assert the_perceptron.predict([0,1]) ==  1, "assert comment"
+    assert the_perceptron.predict([0,0]) ==  0, "assert comment"
