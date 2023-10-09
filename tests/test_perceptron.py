f615e90a45b16c5fb11ea447408f6c288e566312
Ubuntu <ubuntu@ip-172-31-4-96.us-east-2.compute.internal>
2023-10-09 16:34:49 +0000

    test perceptron with unit

--- tests/test_perceptron.py
+++ tests/test_perceptron.py
@@ -1,21 +1,25 @@
 import sys
+import unittest
 sys.path.append("..")
 
 from bin.perceptron_module import Perceptron
 
-def test_perceptron():
-    the_perceptron = Perceptron()
-    the_perceptron.train([
-        [1,1],
-        [1,0],
-        [0,1],
-        [0,0],
-    ], [1,1,1,0])
+class TestPerceptron(unittest.TestCase):
 
-    assert the_perceptron.predict([1,1]) ==  1, "assert comment"
-    assert the_perceptron.predict([1,0]) ==  1, "assert comment"
-    assert the_perceptron.predict([0,1]) ==  1, "assert comment"
-    assert the_perceptron.predict([0,0]) ==  0, "assert comment"
+    def test_perceptron_logic(self):
+        the_perceptron = Perceptron()
+        the_perceptron.train([
+            [1,1],
+            [1,0],
+            [0,1],
+            [0,0],
+        ], [1,1,1,0])
+
+        self.assertEqual(the_perceptron.predict([1,1]), 1, "Expected 1 for input [1,1]")
+        self.assertEqual(the_perceptron.predict([1,0]), 1, "Expected 1 for input [1,0]")
+        self.assertEqual(the_perceptron.predict([0,1]), 1, "Expected 1 for input [0,1]")
+        self.assertEqual(the_perceptron.predict([0,0]), 0, "Expected 0 for input [0,0]")
 
 if __name__ == "__main__":
-    test_perceptron()
+    unittest.main()
+
