dd2adfefee57c5c810eb88b2684755aef13df9ac
Ubuntu <ubuntu@ip-172-31-4-96.us-east-2.compute.internal>
2023-10-09 17:01:58 +0000

    skip if

--- tests/test_perceptron.py
+++ tests/test_perceptron.py
@@ -1,6 +1,8 @@
 import pytest
 import sys
 import unittest
+import os
+import platform
 sys.path.append("..")
 
 from bin.perceptron_module import Perceptron
@@ -22,7 +24,7 @@ class TestPerceptron(unittest.TestCase):
         self.assertEqual(the_perceptron.predict([0,0]), 0, "Expected 0 for input [0,0]")
 
     @pytest.mark.xfail(reason="this test is expected to fail.")
-    def test_perceptron_logic(self):
+    def test_perceptron_failure(self):
         the_perceptron = Perceptron()
         the_perceptron.train([
             [1,1],
@@ -35,6 +37,20 @@ class TestPerceptron(unittest.TestCase):
         self.assertEqual(the_perceptron.predict([1,0]), 1, "Expected 1 for input [1,0]")
         self.assertEqual(the_perceptron.predict([0,1]), 1, "Expected 1 for input [0,1]")
         self.assertEqual(the_perceptron.predict([0,0]), 0, "Expected 0 for input [0,0]")
+
+    @pytest.mark.skipif(platform.system() != "Linux" or "ubuntu" not in platform.version().lower(), reason="Test runs only on Linux Ubuntu.")
+    def test_os_specific_logic(self):
+        # Fetch memory details using os.popen()
+        total_memory, used_memory, free_memory = map(
+            int, os.popen('free -t -m').readlines()[-1].split()[1:])
+        min_memory = 2000  # Example: 2000MB or 2GB
+        # Assert that the total_memory is greater than or equal to the min_memory
+        self.assertGreaterEqual(total_memory, min_memory, f"Expected at least {min_memory/1000} GB of memory. Found only {total_memory/1000} GB.")
+        # You can set desired thresholds for used_memory and free_memory if needed
+        min_used_memory = 500  # Example: 500MB
+        self.assertGreaterEqual(used_memory, min_used_memory, f"Expected at least {min_used_memory/1000} GB of used memory. Found only {used_memory/1000} GB.")
+        min_free_memory = 1000  # Example: 1000MB or 1GB
+        self.assertGreaterEqual(free_memory, min_free_memory, f"Expected at least {min_free_memory/1000} GB of free memory. Found only {free_memory/1000} GB.")
 
 if __name__ == "__main__":
     unittest.main()
