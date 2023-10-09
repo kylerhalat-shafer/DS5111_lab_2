713af78a8cacbf0dd65e34ccbd9d36f7ac244289
Ubuntu <ubuntu@ip-172-31-4-96.us-east-2.compute.internal>
2023-10-09 16:42:01 +0000

    failing test

--- tests/test_perceptron.py
+++ tests/test_perceptron.py
@@ -20,6 +20,20 @@ class TestPerceptron(unittest.TestCase):
         self.assertEqual(the_perceptron.predict([0,1]), 1, "Expected 1 for input [0,1]")
         self.assertEqual(the_perceptron.predict([0,0]), 0, "Expected 0 for input [0,0]")
 
+    def test_perceptron_logic(self):
+        the_perceptron = Perceptron()
+        the_perceptron.train([
+            [1,1],
+            [1,0],
+            [0,1],
+            [0,0],
+        ], [1,1,1,0])
+
+        self.assertNotEqual(the_perceptron.predict([1,1]), 1, "Expected failure for input [1,1]")
+        self.assertEqual(the_perceptron.predict([1,0]), 1, "Expected 1 for input [1,0]")
+        self.assertEqual(the_perceptron.predict([0,1]), 1, "Expected 1 for input [0,1]")
+        self.assertEqual(the_perceptron.predict([0,0]), 0, "Expected 0 for input [0,0]")
+
 if __name__ == "__main__":
     unittest.main()
 
